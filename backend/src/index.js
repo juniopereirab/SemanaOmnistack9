edf5f516e7ec7ac0a283a93b1d6fe1178bc110b7
Damaso Junio <junio.pereirab@gmail.com>
2021-01-04 15:17:06 -0300

    Starting backend

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const server = require('http').Server(app);
+
+mongoose.connect('mongodb+srv://juniopereira:junio123@cluster0.ygj9i.mongodb.net/week9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3001);
