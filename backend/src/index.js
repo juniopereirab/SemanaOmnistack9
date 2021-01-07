b25741027896bdd4a7e2834f70d2e23f98446505
Damaso Junio <junio.pereirab@gmail.com>
2021-01-07 20:56:10 -0300

    Finishing frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://juniopereira:junio123@cluster0.ygj9i.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 server.listen(3001);
